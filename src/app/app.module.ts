5375e93992db3e481f3a13d61c3b71b81337332d
215138848 <80263159+215138848@users.noreply.github.com>
2021-06-29 18:39:33 +0200

    setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExploreComponent } from './explore/explore.component';
+import { LearnComponent } from './learn/learn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    ExploreComponent,
+    LearnComponent
   ],
   imports: [
     BrowserModule
